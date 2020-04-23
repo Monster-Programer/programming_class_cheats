d11633eb1df28b81dbf17fa7d3b7437445ef9882
Monster-Programer <55542266+Monster-Programer@users.noreply.github.com>
2020-04-23 19:35:37 +0430

    all_size_file_hider
    with this change program can hide any file with any size

--- gif_hider/gif_hider.c
+++ gif_hider/gif_hider.c
@@ -1,28 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h> 
+  
+long int findsize(char file_name[]) 
+{
+    FILE* fp = fopen(file_name, "r"); 
+  
+    if (fp == NULL) { 
+        printf("File Not Found!\n"); 
+        return -1; 
+    } 
+  
+    fseek(fp, 0L, SEEK_END); 
+  
+    long int res = ftell(fp); 
+  
+    fclose(fp); 
+  
+    return res; 
+} 
 
-#define MAX_FILE_SIZE (1024*1024)
 int main ()
 {
 	FILE *fptr;
-	char data[MAX_FILE_SIZE];
+	char path[256];
+	char* data;
 
-	fptr = fopen("phpconf.gif", "rb");
+	printf("Enter the path of file : ");
+	scanf("%s",path);
+	long int file_size = findsize(path);
+	
+	data = malloc(file_size);
+
+	fptr = fopen(path,"rb");
 
 	int i = 0;
 	while (!feof(fptr))
 		data[i++] = fgetc(fptr);
 	fclose(fptr);
 
-	fptr = fopen("phpconf.gif", "wb");
+	fptr = fopen(path, "wb");
 
 	for (int j=0; j<i-1; j++)
 		fputc((data[j] ^ 0x60), fptr);
 	fclose(fptr);
+	free(data);
 
 	return 0;
 }
-
-
-
-
-
